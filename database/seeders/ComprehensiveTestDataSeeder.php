41bf7c35d4eeb7314897569d23167e3c18dde782
Alexandre Sonicka Gomah <alexandre.sonicka@akieni.com>
2025-12-23 14:24:40 +0100

    Add import for fake function in ComprehensiveTestDataSeeder

--- database/seeders/ComprehensiveTestDataSeeder.php
+++ database/seeders/ComprehensiveTestDataSeeder.php
@@ -22,6 +22,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use function fake;
 
 class ComprehensiveTestDataSeeder extends Seeder
 {
