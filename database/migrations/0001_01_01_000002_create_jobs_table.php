19a76bbb40e369fb2ed2e5f54f8394ab26e812c1
Alexandre Sonicka Gomah <alexandre.sonicka@akieni.com>
2026-02-18 17:57:50 +0100

    feat: update jobs and failed_jobs migrations to use gen_random_uuid for primary keys
    - Modified the 'id' column in the 'jobs' and 'failed_jobs' tables to use DB::raw('gen_random_uuid()') as the default value, ensuring unique UUIDs are generated automatically upon record creation.

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('jobs', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->uuid('id')->default(DB::raw('gen_random_uuid()'))->primary();
             $table->string('queue')->index();
             $table->longText('payload');
             $table->unsignedTinyInteger('attempts');
@@ -35,7 +36,7 @@ return new class extends Migration
         });
 
         Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->uuid('id')->default(DB::raw('gen_random_uuid()'))->primary();
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
